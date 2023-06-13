27b861eaa78445deaf775c4c4a67c722ee01121e
chen <41134701+Alpha-CL@users.noreply.github.com>
2023-06-13 13:32:30 +0800

    Update utils.py

--- usages/utils.py
+++ usages/utils.py
@@ -1,8 +1,14 @@
 import os
 from dotenv import load_dotenv
+import opencc
 
 
 def load_env():
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     PARENT_DIR = os.path.abspath(os.path.join(BASE_DIR, '..'))
     load_dotenv(os.path.join(PARENT_DIR, '.env'))
+
+
+def traditional_to_simplified(traditional_chinese):
+    converter = opencc.OpenCC("t2s.json")
+    return converter.convert(traditional_chinese)
