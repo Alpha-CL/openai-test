525411b987c95440297edb314c42e809dbb30a09
chen <41134701+Alpha-CL@users.noreply.github.com>
2023-06-13 15:41:11 +0800

    Update __init__.py

--- usages/audio_to_text/__init__.py
+++ usages/audio_to_text/__init__.py
@@ -17,24 +17,25 @@ def validate_file(file):
     file_name = os.path.basename(file.name)
     file_ext = os.path.splitext(file_name)[1]
     ext_list = ['.mp3', '.mp4', '.mpeg', '.mpga', '.m4a', '.wav', '.webm']
-    print(f'================> filename: {file_name}')
-    print(f'================> file_ext: {file_ext}')
+    return file_ext in ext_list
 
 
 # language 参数对应的 code
 # https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
 
 def audio_to_text(file):
-    validate_file(file)
-    transcript = openai.Audio.transcribe(
-        model='whisper-1',
-        file=file,
-        # temperature=0,  # [0,1]
-        # response_format='json',  # json, text, srt
-        # language='zh',  # zh, en, ...
-    )
-    if hasattr(transcript, 'text'):
-        return traditional_to_simplified(transcript.text)
+    if validate_file(file):
+        transcript = openai.Audio.transcribe(
+            model='whisper-1',
+            file=file,
+            # temperature=0,  # [0,1]
+            # response_format='json',  # json, text, srt
+            # language='zh',  # zh, en, ...
+        )
+        if hasattr(transcript, 'text'):
+            return traditional_to_simplified(transcript.text)
+    else:
+        pass
 
 
 if __name__ == '__main__':
