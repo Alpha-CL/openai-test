fa6504bb058c6d50ca41633f40762170a3a7eec2
chen <41134701+Alpha-CL@users.noreply.github.com>
2023-06-13 13:32:12 +0800

    Create handler.py

---
+++ usages/handler.py
@@ -0,0 +1,33 @@
+import signal
+
+import openai
+
+
+class TimeoutException(Exception):
+    pass
+
+
+def timeout_handler(signum, frame):
+    raise TimeoutException()
+
+
+def openai_request(openai_sdk=None, *args, **kwargs):
+    timeout = 30  # 定义超时时间
+    signal.signal(signal.SIGALRM, timeout_handler)  # 注册信号处理程序
+    signal.alarm(timeout)  # 设置超时时间
+
+    try:
+        if openai_sdk is not None and callable(openai_sdk):
+            print("[ Start requesting OpenAI ]")
+            return openai_sdk(*args, **kwargs)
+    except (openai.error.APIError, TimeoutException) as err:
+        if isinstance(err, openai.error.APIError):
+            print(f"[ Request error: {err} ]")
+        else:
+            print("[ OpenAI request timed out ]")
+    else:
+        print("[ Successful request ]")
+    finally:
+        signal.alarm(0)  # 关闭超时计时器
+        print("[ Turn off the timeout timer ]")
+        print("[ Completed request ]")
