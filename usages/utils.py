81e821f98a87a59a0e4380ea3feef9ac3422d4b3
Alphal-CL <xiaodao_92@yeah.net>
2023-06-14 21:44:48 +0800

    feat(main): to lower

--- usages/utils.py
+++ usages/utils.py
@@ -4,9 +4,9 @@ import opencc
 
 
 def load_env():
-    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    PARENT_DIR = os.path.abspath(os.path.join(BASE_DIR, '..'))
-    load_dotenv(os.path.join(PARENT_DIR, '.env'))
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+    parent_dir = os.path.abspath(os.path.join(base_dir, '..'))
+    load_dotenv(os.path.join(parent_dir, '.env'))
 
 
 def traditional_to_simplified(traditional_chinese):
