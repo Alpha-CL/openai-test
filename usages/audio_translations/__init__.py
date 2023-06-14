4d9aecdf6431084c7cd3422ec501f21f04548b9a
chen <41134701+Alpha-CL@users.noreply.github.com>
2023-06-14 10:53:41 +0800

    Update __init__.py

--- usages/audio_translations/__init__.py
+++ usages/audio_translations/__init__.py
@@ -1,18 +1,30 @@
 import os
 import openai
 
+from usages.handler import openai_request
 from usages.utils import load_env
 
 load_env()
 CURRENT_DIR = os.getcwd()
 
-# openai.organization = os.getenv("ORGANIZATION")
-print(f'================> openai.organization : {openai.organization}')
-openai.api_key = os.getenv("OPENAI_API_KEY")
-print(f'================> openai.api_key : {openai.api_key}')
+openai.organization = os.getenv('ORGANIZATION')
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+audio = open(f'{CURRENT_DIR}/audio.mp3', 'rb')
+
+
+def audio_translate(file):
+    translate = openai.Audio.translate(
+        model='whisper-1',
+        file=file,
+        # prompt='',  #
+        # temperature=0,  # [0,1]
+        # response_format='json',  # json, text, srt
+        # language='en',  # zh, en, ...
+    )
+    return translate
+
 
 if __name__ == '__main__':
-    audio_file = open(f"{CURRENT_DIR}/audio.mp3", "rb")
-    print(f'================> audio_file: {audio_file}')
-    transcript = openai.Audio.translate("whisper-1", audio_file)
-    print(f'================> transcript: {transcript}')
+    response = openai_request(audio_translate, file=audio)
+    print(f'================> response: {response}')
